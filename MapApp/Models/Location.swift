bde508100b401897591e8844462f5ff9e65e1f20
MinhVu <minhvulai06@gmail.com>
2024-07-08 19:55:51 +0300

    [Data] Assets and Data Models updated

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Minh Vu Lai on 7/8/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
