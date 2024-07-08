3d1f175677d12d688bd7bf954334989d4159d356
MinhVu <minhvulai06@gmail.com>
2024-07-08 18:50:25 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Minh Vu Lai on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
